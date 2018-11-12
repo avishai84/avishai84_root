75f4e738bc129a26a6bbb1d8a41dab001b3e16fc
Avishai Taashur <avishai@avishai84.com>
2018-11-11 20:22:06 -0800

    linting

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-//import './Header.scss';
 import Navigation from './Navigation.js';
 import 'bootstrap/dist/css/bootstrap.css';
-
 class Header extends Component {
 
   render() {
